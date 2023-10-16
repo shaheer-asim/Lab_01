6a2cf9049a330e2b9c526b456340836f08774881
Shaheer <sp23-bcs-063@cuilahore.edu.pk>
2023-10-16 10:46:48 +0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		PrintIt out = new PrintIt();
+		out.print("Hello");
+	}
+}
---
+++ PrintIt.java
@@ -0,0 +1,6 @@
+public class PrintIt{
+	public void print(String str)
+	{
+		System.out.println(str);
+	}
+}
